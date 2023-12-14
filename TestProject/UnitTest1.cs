e1449a479fd4637e1c26a1e731b0b9e2dccd03d6
ciara-agar <X00174339@myTUDublin.ie>
2023-12-14 11:50:33 +0000

    tests

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -21,7 +21,7 @@ public class SidebarTests
     {
         _driver = new ChromeDriver();
         wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-        _driver.Navigate().GoToUrl("https://daviddunne165.github.io/EAD-CA3-MatchScore/");
+        _driver.Navigate().GoToUrl("http://localhost:5253/");
     }
 
     [TestCleanup]
@@ -35,5 +35,46 @@ public class SidebarTests
     {
         var sidebarElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("sidebar")));
         Assert.IsNotNull(sidebarElement);
+    }
+
+    [TestMethod]
+    public void Index_Loads_Correctly()
+    {
+        var indexElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.ClassName("center-content")));
+        Assert.IsNotNull(indexElement);
+    }
+
+    [TestMethod]
+    public void Search_Clicks_Correctly()
+    {
+        var searchElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//a[@href='spotify-search']")));
+        Assert.IsNotNull(searchElement);
+    }
+
+    [TestMethod]
+    public void Search_Button_Clicks_Correctly()
+    {
+        var searchElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//a[@href='spotify-search']")));
+        searchElement.Click();
+        var searchButElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//button[text()='Search']")));
+        searchButElement.Click();
+        Assert.IsNotNull(searchButElement);
+    }
+
+    [TestMethod]
+    public void SearchUser_Clicks_Correctly()
+    {
+        var searchUserElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//a[@href='user']")));
+        Assert.IsNotNull(searchUserElement);
+    }
+
+    [TestMethod]
+    public void SearchUser_Button_Clicks_Correctly()
+    {
+        var searchUserElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//a[@href='user']")));
+        searchUserElement.Click();
+        var searchUserButElement =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath("//button[text()='Search']")));
+        searchUserButElement.Click();
+        Assert.IsNotNull(searchUserButElement);
     }
 }
